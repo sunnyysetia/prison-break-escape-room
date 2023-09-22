32805947724a44b1f79780eb74102d3f6f4445f3
sunnyysetia <89380644+sunnyysetia@users.noreply.github.com>
2023-09-22 13:25:21 +1200

    feat: Added banging on door sound effect to begin game loading screen

--- src/main/java/nz/ac/auckland/se206/uils/SoundUtils.java
+++ src/main/java/nz/ac/auckland/se206/uils/SoundUtils.java
@@ -4,6 +4,8 @@ import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
 public class SoundUtils {
+    private static MediaPlayer currentMediaPlayer;
+
     public static void playSound(String fileName) {
         try {
             // Build the resource path to the sound file
@@ -12,12 +14,24 @@ public class SoundUtils {
             // Create a Media object with the resource path
             Media sound = new Media(SoundUtils.class.getResource(resourcePath).toString());
 
-            // Create a MediaPlayer with the Media object
-            MediaPlayer mediaPlayer = new MediaPlayer(sound);
-            mediaPlayer.setVolume(0.1);
+            // Create a new MediaPlayer with the Media object
+            MediaPlayer newMediaPlayer = new MediaPlayer(sound);
+            newMediaPlayer.setVolume(0.1);
+
+            // Check if there is a currently playing sound
+            if (currentMediaPlayer != null && currentMediaPlayer.getStatus() == MediaPlayer.Status.PLAYING) {
+                // A sound is already playing, do nothing
+                return;
+            }
 
             // Play the sound effect
-            mediaPlayer.play();
+            newMediaPlayer.play();
+
+            // Update the currentMediaPlayer
+            currentMediaPlayer = newMediaPlayer;
+
+            // Set an event handler to clear the currentMediaPlayer when playback ends
+            currentMediaPlayer.setOnEndOfMedia(() -> currentMediaPlayer = null);
         } catch (Exception e) {
             // Handle any exceptions that may occur (e.g., file not found)
             e.printStackTrace();
